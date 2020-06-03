269cccc46bb1e1ca87ae2cc2b3c1a2b89baf8ab5
amidevous <amidevous@users.noreply.github.com>
2020-06-04 01:31:59 +0200

    update
    update

---
+++ install/updatebeta.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+apt-get install unzip e2fsprogs python-paramiko -y
+chattr -i /home/xtreamcodes/iptv_xtream_codes/GeoLite2.mmdb
+rm -rf /home/xtreamcodes/iptv_xtream_codes/admin
+rm -rf /home/xtreamcodes/iptv_xtream_codes/pytools
+rm -rf /home/xtreamcodes/iptv_xtream_codes/adtools
+wget "http://strong-livetv.com/install/release_21_official.zip" -O /tmp/update.zip
+unzip /tmp/update.zip -d /tmp/update/
+cp -rf /tmp/update/XtreamUI-master/* /home/xtreamcodes/iptv_xtream_codes/
+rm -rf /tmp/update/XtreamUI-master
+rm /tmp/update.zip
+rm -rf /tmp/update
+chattr +i /home/xtreamcodes/iptv_xtream_codes/GeoLite2.mmdb
+killall nginx
+killall nginx_rtmp
+killall php-fpm
+cd /home/xtreamcodes/iptv_xtream_codes/crons
+rm -f pid_monitor.php
+wget "http://strong-livetv.com/install/pid_monitor.zip" -O pid_monitor.zip
+unzip pid_monitor.zip
+rm -f pid_monitor.zip
+sed -i 's|true|false|' /home/xtreamcodes/iptv_xtream_codes/admin/header_sidebar.php
+sed -i 's|} else { echo "checked "; }|}|' /home/xtreamcodes/iptv_xtream_codes/admin/episode.php
+sed -i 's|} else { echo "checked "; }|}|' /home/xtreamcodes/iptv_xtream_codes/admin/movie.php
+sed -i 's|$rSessionTimeout = 60;|$rSessionTimeout = 6000;|' /home/xtreamcodes/iptv_xtream_codes/admin/session.php
+sed -i 's|$rSessionTimeout*60|$rSessionTimeout*6000|' /home/xtreamcodes/iptv_xtream_codes/admin/session.php
+sudo chown -R xtreamcodes:xtreamcodes /home/xtreamcodes/
+chmod +x /home/xtreamcodes/iptv_xtream_codes/nginx_rtmp/sbin/nginx_rtmp
+chmod +x /home/xtreamcodes/iptv_xtream_codes/nginx/sbin/nginx
+/home/xtreamcodes/iptv_xtream_codes/start_services.sh
+wget http://strong-livetv.com/install/settings.php.txt -O /home/xtreamcodes/iptv_xtream_codes/admin/settings.php
