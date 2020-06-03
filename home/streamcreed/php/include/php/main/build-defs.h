42ca7b6322fc945a5b4478f0ab9568649b364422
amidevous <amidevous@users.noreply.github.com>
2020-06-03 21:44:15 +0200

    update
    update

---
+++ home/streamcreed/php/include/php/main/build-defs.h
@@ -0,0 +1,41 @@
+/*
+   +----------------------------------------------------------------------+
+   | PHP Version 7                                                        |
+   +----------------------------------------------------------------------+
+   | Copyright (c) The PHP Group                                          |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 3.01 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available through the world-wide-web at the following url:           |
+   | http://www.php.net/license/3_01.txt                                  |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Author: Stig Sæther Bakken <ssb@php.net>                             |
+   +----------------------------------------------------------------------+
+*/
+
+#define CONFIGURE_COMMAND " './configure'  '--enable-mbstring' '--enable-static' '--disable-shared' '--enable-opcache' '--enable-fpm' '--without-sqlite3' '--without-pdo-sqlite' '--prefix=/home/streamcreed/php' '--enable-mysqlnd' '--with-mysqli' '--with-curl' '--disable-cgi' '--with-zlib' '--enable-sockets' '--with-openssl' '--enable-shmop' '--enable-sysvsem' '--enable-sysvshm' '--enable-sysvmsg'"
+#define PHP_ODBC_CFLAGS	""
+#define PHP_ODBC_LFLAGS		""
+#define PHP_ODBC_LIBS		""
+#define PHP_ODBC_TYPE		""
+#define PHP_OCI8_DIR			""
+#define PHP_OCI8_ORACLE_VERSION		""
+#define PHP_PROG_SENDMAIL	""
+#define PEAR_INSTALLDIR         ""
+#define PHP_INCLUDE_PATH	".:"
+#define PHP_EXTENSION_DIR       "/home/streamcreed/php/lib/php/extensions/no-debug-non-zts-20190902"
+#define PHP_PREFIX              "/home/streamcreed/php"
+#define PHP_BINDIR              "/home/streamcreed/php/bin"
+#define PHP_SBINDIR             "/home/streamcreed/php/sbin"
+#define PHP_MANDIR              "/home/streamcreed/php/php/man"
+#define PHP_LIBDIR              "/home/streamcreed/php/lib/php"
+#define PHP_DATADIR             "/home/streamcreed/php/share/php"
+#define PHP_SYSCONFDIR          "/home/streamcreed/php/etc"
+#define PHP_LOCALSTATEDIR       "/home/streamcreed/php/var"
+#define PHP_CONFIG_FILE_PATH    "/home/streamcreed/php/lib"
+#define PHP_CONFIG_FILE_SCAN_DIR    ""
+#define PHP_SHLIB_SUFFIX        "so"
+#define PHP_SHLIB_EXT_PREFIX    ""
