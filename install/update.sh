7957c578731561af5a9b9a7332da82fe7d15f012
amidevous <amidevous@users.noreply.github.com>
2020-06-04 01:46:08 +0200

    Update update.sh

--- install/update.sh
+++ install/update.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
 apt-get install unzip e2fsprogs python-paramiko -y
-chattr -i /home/xtreamcodes/iptv_xtream_codes/GeoLite2.mmdb
-rm -rf /home/xtreamcodes/iptv_xtream_codes/admin
-rm -rf /home/xtreamcodes/iptv_xtream_codes/pytools
-rm -rf /home/xtreamcodes/iptv_xtream_codes/adtools
-wget "http://strong-livetv.com/install/release_18_official.zip" -O /tmp/update.zip
+#chattr -i /home/streamcreed/GeoLite2.mmdb
+rm -rf /home/streamcreed/admin
+rm -rf /home/streamcreed/pytools
+rm -rf /home/streamcreed/adtools
+wget "https://github.com/amidevous/xtream-ui-beta-install/raw/master/install/release_18_official.zip" -O /tmp/update.zip
 unzip /tmp/update.zip -d /tmp/update/
-cp -rf /tmp/update/XtreamUI-master/* /home/xtreamcodes/iptv_xtream_codes/
-rm -rf /tmp/update/XtreamUI-master
+rm -rf /tmp/update/php
+cp -rf /tmp/update/XtreamUI-master/* /home/streamcreed
+rm -rf /tmp/update/
 rm /tmp/update.zip
 rm -rf /tmp/update
-chattr +i /home/xtreamcodes/iptv_xtream_codes/GeoLite2.mmdb
+#chattr +i /home/streamcreed/GeoLite2.mmdb
 killall nginx
-killall nginx_rtmp
+killall nginx
 killall php-fpm
-cd /home/xtreamcodes/iptv_xtream_codes/crons
+cd /home/streamcreed/crons
 rm -f pid_monitor.php
-wget "http://strong-livetv.com/install/pid_monitor.zip" -O pid_monitor.zip
+wget "https://github.com/amidevous/xtream-ui-beta-install/raw/master/install/pid_monitor.zip" -O pid_monitor.zip
 unzip pid_monitor.zip
 rm -f pid_monitor.zip
-sed -i 's|true|false|' /home/xtreamcodes/iptv_xtream_codes/admin/header_sidebar.php
-sed -i 's|} else { echo "checked "; }|}|' /home/xtreamcodes/iptv_xtream_codes/admin/episode.php
-sed -i 's|} else { echo "checked "; }|}|' /home/xtreamcodes/iptv_xtream_codes/admin/movie.php
-sed -i 's|$rSessionTimeout = 60;|$rSessionTimeout = 6000;|' /home/xtreamcodes/iptv_xtream_codes/admin/session.php
-sed -i 's|$rSessionTimeout*60|$rSessionTimeout*6000|' /home/xtreamcodes/iptv_xtream_codes/admin/session.php
-sudo chown -R xtreamcodes:xtreamcodes /home/xtreamcodes/
-chmod +x /home/xtreamcodes/iptv_xtream_codes/nginx_rtmp/sbin/nginx_rtmp
-chmod +x /home/xtreamcodes/iptv_xtream_codes/nginx/sbin/nginx
-/home/xtreamcodes/iptv_xtream_codes/start_services.sh
-wget http://strong-livetv.com/install/settings.php.txt -O /home/xtreamcodes/iptv_xtream_codes/admin/settings.php
+sed -i 's|true|false|' /home/streamcreed/admin/header_sidebar.php
+sed -i 's|} else { echo "checked "; }|}|' /home/streamcreed/admin/episode.php
+sed -i 's|} else { echo "checked "; }|}|' /home/streamcreed/admin/movie.php
+sed -i 's|$rSessionTimeout = 60;|$rSessionTimeout = 6000;|' /home/streamcreed/admin/session.php
+sed -i 's|$rSessionTimeout*60|$rSessionTimeout*6000|' /home/streamcreed/admin/session.php
+sudo chown -R streamcreed:streamcreed /home/streamcreed/
+chmod -R 0777 /home/streamcreed
+chmod +x /home/streamcreed/nginx_rtmp/sbin/nginx
+chmod +x /home/streamcreed/nginx/sbin/nginx
+/home/streamcreed/start_services.sh
+wget https://github.com/amidevous/xtream-ui-beta-install/raw/master/install/settings.php.txt -O /home/streamcreed/admin/settings.php
