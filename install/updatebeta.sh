306cc1c145dcf51f2dc600d717a3c21366237628
amidevous <amidevous@users.noreply.github.com>
2020-06-08 13:06:05 +0200

    Update updatebeta.sh

--- install/updatebeta.sh
+++ install/updatebeta.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 apt-get install unzip e2fsprogs python-paramiko -y
-#chattr -i /home/streamcreed/GeoLite2.mmdb
-rm -rf /home/streamcreed/admin
-rm -rf /home/streamcreed/pytools
-rm -rf /home/streamcreed/adtools
+chattr -i /home/xtreamcodes/iptv_xtream_codes/GeoLite2.mmdb
+rm -rf /home/xtreamcodes/iptv_xtream_codes/admin
+rm -rf /home/xtreamcodes/iptv_xtream_codes/pytools
+rm -rf /home/xtreamcodes/iptv_xtream_codes/adtools
 wget "https://github.com/amidevous/xtream-ui-beta-install/raw/master/install/release_21_official.zip" -O /tmp/update.zip
 unzip /tmp/update.zip -d /tmp/update/
-rm -rf /tmp/update/php
-cp -rf /tmp/update/XtreamUI-master/* /home/streamcreed
+cp -rf /tmp/update/XtreamUI-master/* /home/xtreamcodes/iptv_xtream_codes/
+rm -rf /tmp/update/XtreamUI-master
+rm /tmp/update.zip
 rm -rf /tmp/update
-#chattr +i /home/streamcreed/GeoLite2.mmdb
+chattr +i /home/xtreamcodes/iptv_xtream_codes/GeoLite2.mmdb
 killall nginx
-killall nginx
+killall nginx_rtmp
 killall php-fpm
-cd /home/streamcreed/crons
+cd /home/xtreamcodes/iptv_xtream_codes/crons
 rm -f pid_monitor.php
 wget "https://github.com/amidevous/xtream-ui-beta-install/raw/master/install/pid_monitor.zip" -O pid_monitor.zip
 unzip pid_monitor.zip
 rm -f pid_monitor.zip
-sed -i 's|true|false|' /home/streamcreed/admin/header_sidebar.php
-sed -i 's|} else { echo "checked "; }|}|' /home/streamcreed/admin/episode.php
-sed -i 's|} else { echo "checked "; }|}|' /home/streamcreed/admin/movie.php
-sed -i 's|$rSessionTimeout = 60;|$rSessionTimeout = 6000;|' /home/streamcreed/admin/session.php
-sed -i 's|$rSessionTimeout*60|$rSessionTimeout*6000|' /home/streamcreed/admin/session.php
-sudo chown -R streamcreed:streamcreed /home/streamcreed/
-chmod -R 0777 /home/streamcreed
-chmod +x /home/streamcreed/nginx_rtmp/sbin/nginx
-chmod +x /home/streamcreed/nginx/sbin/nginx
-/home/streamcreed/start_services.sh
-wget https://github.com/amidevous/xtream-ui-beta-install/raw/master/install/settings.php.txt -O /home/streamcreed/admin/settings.php
+sed -i 's|true|false|' /home/xtreamcodes/iptv_xtream_codes/admin/header_sidebar.php
+sed -i 's|} else { echo "checked "; }|}|' /home/xtreamcodes/iptv_xtream_codes/admin/episode.php
+sed -i 's|} else { echo "checked "; }|}|' /home/xtreamcodes/iptv_xtream_codes/admin/movie.php
+sed -i 's|$rSessionTimeout = 60;|$rSessionTimeout = 6000;|' /home/xtreamcodes/iptv_xtream_codes/admin/session.php
+sed -i 's|$rSessionTimeout*60|$rSessionTimeout*6000|' /home/xtreamcodes/iptv_xtream_codes/admin/session.php
+sudo chown -R xtreamcodes:xtreamcodes /home/xtreamcodes/
+chmod +x /home/xtreamcodes/iptv_xtream_codes/nginx_rtmp/sbin/nginx_rtmp
+chmod +x /home/xtreamcodes/iptv_xtream_codes/nginx/sbin/nginx
+/home/xtreamcodes/iptv_xtream_codes/start_services.sh
+wget https://github.com/amidevous/xtream-ui-beta-install/raw/master/install/settings.php.txt -O /home/xtreamcodes/iptv_xtream_codes/admin/settings.php
