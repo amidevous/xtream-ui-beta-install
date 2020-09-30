d8f2f743d91fd9ce60737b2b722a59a6e6a5ab19
amidevous <amidevous@users.noreply.github.com>
2020-09-30 21:02:55 +0200

    Update update.sh

--- install/update.sh
+++ install/update.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 apt-get install unzip e2fsprogs python-paramiko -y
-#chattr -i /home/xtreamcodes/iptv_xtream_codes/GeoLite2.mmdb
-rm -rf /home/streamcreed/admin
-rm -rf /home/streamcreed/pytools
-rm -rf /home/streamcreed/adtools
+chattr -i /home/xtreamcodes/iptv_xtream_codes/GeoLite2.mmdb
+rm -rf /home/xtreamcodes/iptv_xtream_codesiptv_xtream_codes/admin
+rm -rf /home/xtreamcodes/iptv_xtream_codesiptv_xtream_codes/pytools
+rm -rf /home/xtreamcodes/iptv_xtream_codesiptv_xtream_codes/adtools
 wget "https://github.com/amidevous/xtream-ui/archive/r21.zip" -O /tmp/update.zip
 unzip /tmp/update.zip -d /tmp/update/
 cp -rf /tmp/update/xtream-ui-r21/* /home/xtreamcodes/iptv_xtream_codesiptv_xtream_codes
 rm -rf /tmp/update/xtream-ui-r21
 rm /tmp/update.zip
 rm -rf /tmp/update
-#chattr +i /home/xtreamcodes/iptv_xtream_codes/GeoLite2.mmdb
+chattr +i /home/xtreamcodes/iptv_xtream_codes/GeoLite2.mmdb
 killall nginx
 killall nginx_rtmp
 killall php-fpm
