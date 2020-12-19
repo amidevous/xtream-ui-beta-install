c91aaef7411753f820b9dc8fd21b734d4e52968a
amidevous <amidevous@users.noreply.github.com>
2020-12-19 14:34:27 +0100

    Create install-service.sh

---
+++ install/install-service.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+update-rc.d -f xtreamcodes remove
+systemctl disable xtreamcodes
+rm -f /etc/init.d/xtreamcodes
+apt-get -y install cron
+if ! grep -q "/home/xtreamcodes/iptv_xtream_codes/start_services.sh" /etc/crontab; then
+    echo "@reboot root /home/xtreamcodes/iptv_xtream_codes/start_services.sh" >> /etc/crontab
+EOF
+fi
+systemctl enable cron
